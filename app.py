b75e6b759c5c78651bf10ee1cc95d0fecfcc9628
Shivang Nagta <shivangnag@gmail.com>
2024-05-23 15:14:26 +0530

    Page for adding teams added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 Entrypoint for our management portal
 Author: Team SoftCom
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from lib.models import *
 
@@ -10,9 +10,37 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
+# Storing team details
+team = []
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+# Page for adding the teams (Shivang)
+@app.route('/addTeam', methods=["GET", "POST"])
+def addTeam():
+    if request.method == "POST":
+        newTeam = {
+            "teamName": request.form["teamName"],
+            "teamMembers": request.form["teamMembers"], 
+        }
+        if newTeam not in team:
+            team.append(newTeam)
+            print(team)
+        return redirect(url_for('addTeam')) 
+    return render_template("addTeam.html", teams=team)
+
+# Removing the teams (Shivang)
+@app.route('/removeTeam', methods=['POST'])
+def removeTeam():
+    teamName = request.form['teamName']
+    global team
+    team = [t for t in team if t['teamName'] != teamName]
+    print(f"After removal: {team}")  
+    return redirect(url_for('addTeam'))
+
 
 with app.app_context():
     db.create_all()

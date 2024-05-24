a8f88ab39f745178d80ecdda6d5e72d1849766ad
Nalin Angrish <nalin@nalinangrish.me>
2024-05-24 16:07:58 +0530

    Code formatting. Baaki sab badhiya

--- app.py
+++ app.py
@@ -2,66 +2,73 @@
 Entrypoint for our management portal
 Author: Team SoftCom
 """
+
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from lib.models import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
+
 
 # Competitions are stored as object of this class
 class CompetitionObj:
-    def __init__(self,competitionName,numberOfRounds,walletSize):
-        self.competitionName=competitionName
-        self.numberOfRounds=numberOfRounds
-        self.walletSize=walletSize
-        self.stocks={}
-        self.rounds={}
-
-# Competition - contains all the added compeititions as (key: Competition Name, value: corresponding object)       
-Competition={}
+    def __init__(self, competitionName, numberOfRounds, walletSize):
+        self.competitionName = competitionName
+        self.numberOfRounds = numberOfRounds
+        self.walletSize = walletSize
+        self.stocks = {}
+        self.rounds = {}
 
 
-@app.route('/', methods=['GET', 'POST'])
+# Competition - contains all the added compeititions as (key: Competition Name, value: corresponding object)
+Competition = {}
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # Stock_Admin - portal to add a Competition and involved elements (stocks, wallet size etc)
-@app.route('/stock_admin', methods=['GET', 'POST'])
+@app.route("/stock_admin", methods=["GET", "POST"])
 def stock_admin():
-    if request.method == 'POST':
-        competitionName = request.form.get('competitionName')
-        numberOfRounds = int(request.form.get('rounds'))
-        walletSize =request.form.get('walletSize')
-        obj=CompetitionObj(competitionName, numberOfRounds, walletSize)
+    if request.method == "POST":
+        competitionName = request.form.get("competitionName")
+        numberOfRounds = int(request.form.get("rounds"))
+        walletSize = request.form.get("walletSize")
+        obj = CompetitionObj(competitionName, numberOfRounds, walletSize)
 
-        stockNames = request.form.getlist('stockName')
-        priceList= request.form.getlist('price')
+        stockNames = request.form.getlist("stockName")
+        priceList = request.form.getlist("price")
 
         for i in range(len(stockNames)):
-            obj.stocks[stockNames[i]]=priceList[i]
-            obj.rounds[stockNames[i]]=request.form.getlist(f'round-{stockNames[i]}')
+            obj.stocks[stockNames[i]] = priceList[i]
+            obj.rounds[stockNames[i]] = request.form.getlist(f"round-{stockNames[i]}")
 
-        Competition[competitionName]=obj
-        return redirect('/stock_admin')
-    return render_template('stock_admin.html', competition=Competition)
+        Competition[competitionName] = obj
+        return redirect("/stock_admin")
+    return render_template("stock_admin.html", competition=Competition)
+
 
 # Dashboard to view all added Competitions (and the involved elements - Stocks, Prices etc)
-@app.route('/dashboard', methods=["GET","POST"])
+@app.route("/dashboard", methods=["GET", "POST"])
 def receive_data():
-    return render_template('dashboard.html', competition=Competition)
+    return render_template("dashboard.html", competition=Competition)
+
 
 # Delete Competition from Dashboard
-@app.route('/delete-competition', methods=['POST'])
+@app.route("/delete-competition", methods=["POST"])
 def delete_competition():
-    key_to_delete = request.form.get('keyToDelete')
+    key_to_delete = request.form.get("keyToDelete")
     if key_to_delete in Competition:
         del Competition[key_to_delete]
-    return redirect('/dashboard')
+    return redirect("/dashboard")
+
 
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- static/stock_admin.js
+++ static/stock_admin.js
@@ -4,58 +4,58 @@
 let stockEntries = [];
 
 // The first stock input field row is created using onload
-window.onload = function() {
-var initialDiv = document.getElementById("stockInputsContainer");
+window.onload = function () {
+  var initialDiv = document.getElementById("stockInputsContainer");
 
-var inputDiv = document.createElement("div");
-inputDiv.className = "flex items-center mb-2";
+  var inputDiv = document.createElement("div");
+  inputDiv.className = "flex items-center mb-2";
 
-var inputFields = createInputField();
-inputDiv.appendChild(inputFields[0]);
-inputDiv.appendChild(inputFields[1]);
+  var inputFields = createInputField();
+  inputDiv.appendChild(inputFields[0]);
+  inputDiv.appendChild(inputFields[1]);
 
-var addButton = createButton("Add Stock", function() {
+  var addButton = createButton("Add Stock", function () {
     addInputField(this);
-});
+  });
 
-inputDiv.appendChild(addButton);
-initialDiv.appendChild(inputDiv);
+  inputDiv.appendChild(addButton);
+  initialDiv.appendChild(inputDiv);
 };
 
 
 // Function to create an input stock field with specified attributes
 function createInputField() {
-var stockNameInput = document.createElement("input");
-stockNameInput.type = "text";
-stockNameInput.className = "w-2/5 px-3 py-2 border rounded-md mr-2 bg-gray-900";
-stockNameInput.placeholder = "Stock Name";
+  var stockNameInput = document.createElement("input");
+  stockNameInput.type = "text";
+  stockNameInput.className = "w-2/5 px-3 py-2 border rounded-md mr-2 bg-gray-900";
+  stockNameInput.placeholder = "Stock Name";
 
-var priceInputContainer = document.createElement("div");
-priceInputContainer.className = "relative w-1/5 mr-2";
+  var priceInputContainer = document.createElement("div");
+  priceInputContainer.className = "relative w-1/5 mr-2";
 
-var rupeeSymbol = document.createElement("span");
-rupeeSymbol.className = "absolute inset-y-0 left-0 flex items-center pl-2 text-lg";
-rupeeSymbol.innerHTML = "&#x20b9;";
+  var rupeeSymbol = document.createElement("span");
+  rupeeSymbol.className = "absolute inset-y-0 left-0 flex items-center pl-2 text-lg";
+  rupeeSymbol.innerHTML = "&#x20b9;";
 
-var priceInput = document.createElement("input");
-priceInput.type = "number";
-priceInput.className = "pl-8 py-2 border rounded-md w-full bg-gray-900";
-priceInput.placeholder = "Price";
+  var priceInput = document.createElement("input");
+  priceInput.type = "number";
+  priceInput.className = "pl-8 py-2 border rounded-md w-full bg-gray-900";
+  priceInput.placeholder = "Price";
 
-priceInputContainer.appendChild(rupeeSymbol);
-priceInputContainer.appendChild(priceInput);
+  priceInputContainer.appendChild(rupeeSymbol);
+  priceInputContainer.appendChild(priceInput);
 
-return [stockNameInput, priceInputContainer];
+  return [stockNameInput, priceInputContainer];
 }
 
 // function to create Buttons (Add Stock, Remove specficically)
 function createButton(label, onClick, additionalClasses = "bg-blue-500 text-white px-4 py-2 rounded-md w-1/6 ml-auto") {
-var button = document.createElement("button");
-button.innerHTML = label;
-button.type = "button";
-button.onclick = onClick;
-button.className = additionalClasses;
-return button;
+  var button = document.createElement("button");
+  button.innerHTML = label;
+  button.type = "button";
+  button.onclick = onClick;
+  button.className = additionalClasses;
+  return button;
 }
 
 // function to create a new input field once add stock clicked
@@ -63,63 +63,63 @@ return button;
 //      disables input fields once added
 //      prevents add stock from working if no user input
 function addInputField(button) {
-var parentDiv = button.parentNode;
-var stockNameInput = parentDiv.querySelector('input[type="text"]');
-var priceInput = parentDiv.querySelector('input[type="number"]');
+  var parentDiv = button.parentNode;
+  var stockNameInput = parentDiv.querySelector('input[type="text"]');
+  var priceInput = parentDiv.querySelector('input[type="number"]');
 
-// Get the values from the input fields
-var stockName = stockNameInput.value;
-var price = priceInput.value;
-stockNameInput.name = "stockName";
-stockNameInput.readOnly = true;
-stockNameInput.style.border = "none";
-priceInput.name="price"; 
-priceInput.readOnly = true;
-priceInput.style.border = "none";
+  // Get the values from the input fields
+  var stockName = stockNameInput.value;
+  var price = priceInput.value;
+  stockNameInput.name = "stockName";
+  stockNameInput.readOnly = true;
+  stockNameInput.style.border = "none";
+  priceInput.name = "price";
+  priceInput.readOnly = true;
+  priceInput.style.border = "none";
 
-// Add the values to the stockEntries array
-if (stockName && price) {
+  // Add the values to the stockEntries array
+  if (stockName && price) {
     stockEntries.push({ name: stockName, price: price });
     console.log(stockEntries);
-} else {
-        alert("Please enter both stock name and price.");
+  } else {
+    alert("Please enter both stock name and price.");
     return;
-}
+  }
 
-//Remove button has two functions 
-//  - to only remove one stock field
-//  - or if closing price entries have been given, then to also remove closing price row for corresponding stock selectively 
-var delButton= createButton("Remove", function() {
+  //Remove button has two functions 
+  //  - to only remove one stock field
+  //  - or if closing price entries have been given, then to also remove closing price row for corresponding stock selectively 
+  var delButton = createButton("Remove", function () {
     var index = stockEntries.findIndex(entry => entry.name === stockName && entry.price == price);
     if (index !== -1) {
-        stockEntries.splice(index, 1);
-        console.log(stockEntries);
-        // displayStockNames(stockEntries.length);
+      stockEntries.splice(index, 1);
+      console.log(stockEntries);
+      // displayStockNames(stockEntries.length);
     }
-    if ( document.getElementById("closingPrices").style.display=="block") {
-        removeStock(stockName);
-        //remove closing price row for corresponding stock selectively if its being used
+    if (document.getElementById("closingPrices").style.display == "block") {
+      removeStock(stockName);
+      //remove closing price row for corresponding stock selectively if its being used
     }
     parentDiv.remove();
 
-}, "bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded-md w-1/6 ml-auto");
+  }, "bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded-md w-1/6 ml-auto");
 
-parentDiv.removeChild(button);
-parentDiv.appendChild(delButton);
+  parentDiv.removeChild(button);
+  parentDiv.appendChild(delButton);
 
-var newRowDiv = document.createElement("div");
-newRowDiv.className = "flex items-center mb-2";
+  var newRowDiv = document.createElement("div");
+  newRowDiv.className = "flex items-center mb-2";
 
-var inputFields = createInputField();
-newRowDiv.appendChild(inputFields[0]);
-newRowDiv.appendChild(inputFields[1]);
+  var inputFields = createInputField();
+  newRowDiv.appendChild(inputFields[0]);
+  newRowDiv.appendChild(inputFields[1]);
 
-var addButton =createButton("Add Stock", function() {
+  var addButton = createButton("Add Stock", function () {
     addInputField(this);
-});
-newRowDiv.appendChild(addButton);
+  });
+  newRowDiv.appendChild(addButton);
 
-parentDiv.parentNode.appendChild(newRowDiv);
+  parentDiv.parentNode.appendChild(newRowDiv);
 }
 
 
@@ -127,51 +127,51 @@ parentDiv.parentNode.appendChild(newRowDiv);
 // Closing Prices:
 // remove closing price row for corresponding stock selectively
 function removeStock(stockName) {
-// Remove the stock name element
-const stockItems = document.querySelectorAll(`.stock-item[stock="${stockName}"]`);
-stockItems.forEach(item => item.remove());
+  // Remove the stock name element
+  const stockItems = document.querySelectorAll(`.stock-item[stock="${stockName}"]`);
+  stockItems.forEach(item => item.remove());
 }
 
 
 // Closing Price Fields generation: (takes stock names automaitcally from filled user entries)
 function displayStockNames() {
-document.getElementById("closingPrices").style.display="block";
-var displayContainer = document.getElementById("stockDisplayContainer");
-var x = document.getElementById("rounds").value;
-displayContainer.innerHTML = ""; // Clear previous entries
-for (var j=1; j<=x; j++){
+  document.getElementById("closingPrices").style.display = "block";
+  var displayContainer = document.getElementById("stockDisplayContainer");
+  var x = document.getElementById("rounds").value;
+  displayContainer.innerHTML = ""; // Clear previous entries
+  for (var j = 1; j <= x; j++) {
     var roundEntry = document.createElement("div");
-        roundEntry.className = "mb-2";
-        roundEntry.innerHTML = '<label for="rounds" class="block text-white-700 font mb-2 ml-2">Round '+j.toString() + ' - </label';
-        displayContainer.appendChild(roundEntry);
-    for (var i = 0;i < stockEntries.length; i++) {
-        var row=document.createElement("div");
-        row.className="stock-item flex items-center mb-2";
-        row.setAttribute("stock", stockEntries[i].name); 
-        var stockEntry = document.createElement("div");
-        stockEntry.className = "mb-2 ml-4 mr-2 w-1/3";
-        stockEntry.innerHTML = stockEntries[i].name + ' : ';
-        row.appendChild(stockEntry);
-        var priceInputContainer = document.createElement("div");
-        priceInputContainer.className = "relative w-1/5 mr-2";
+    roundEntry.className = "mb-2";
+    roundEntry.innerHTML = '<label for="rounds" class="block text-white-700 font mb-2 ml-2">Round ' + j.toString() + ' - </label';
+    displayContainer.appendChild(roundEntry);
+    for (var i = 0; i < stockEntries.length; i++) {
+      var row = document.createElement("div");
+      row.className = "stock-item flex items-center mb-2";
+      row.setAttribute("stock", stockEntries[i].name);
+      var stockEntry = document.createElement("div");
+      stockEntry.className = "mb-2 ml-4 mr-2 w-1/3";
+      stockEntry.innerHTML = stockEntries[i].name + ' : ';
+      row.appendChild(stockEntry);
+      var priceInputContainer = document.createElement("div");
+      priceInputContainer.className = "relative w-1/5 mr-2";
 
-        var rupeeSymbol = document.createElement("span");
-        rupeeSymbol.className = "absolute inset-y-0 left-0 flex items-center pl-2 text-lg";
-        rupeeSymbol.innerHTML = "&#x20b9;";
+      var rupeeSymbol = document.createElement("span");
+      rupeeSymbol.className = "absolute inset-y-0 left-0 flex items-center pl-2 text-lg";
+      rupeeSymbol.innerHTML = "&#x20b9;";
 
-        var priceInput = document.createElement("input");
-        priceInput.type = "number";
-        priceInput.className = "pl-8 py-2 border rounded-md w-full bg-gray-900";
-        priceInput.name="round-"+stockEntries[i].name;
-        priceInput.placeholder = "Price";
+      var priceInput = document.createElement("input");
+      priceInput.type = "number";
+      priceInput.className = "pl-8 py-2 border rounded-md w-full bg-gray-900";
+      priceInput.name = "round-" + stockEntries[i].name;
+      priceInput.placeholder = "Price";
 
-        priceInputContainer.appendChild(rupeeSymbol);
-        priceInputContainer.appendChild(priceInput);
-        row.appendChild(priceInputContainer);    
+      priceInputContainer.appendChild(rupeeSymbol);
+      priceInputContainer.appendChild(priceInput);
+      row.appendChild(priceInputContainer);
 
-        roundEntry.appendChild(row);
+      roundEntry.appendChild(row);
     }
-}
+  }
 }
 
 

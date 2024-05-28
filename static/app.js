2b095c8326375050b4771f11c15195d7aa6824ec
Keshav Arora <keshav11y@gmail.com>
2024-05-28 11:01:15 +0530

    Updated styling using Tailwind CSS

--- static/app.js
+++ static/app.js
@@ -1,33 +1,33 @@
-let form = document.getElementById('form');
-let content = document.getElementById('content');
-let title = document.getElementById('title');
-let img = document.getElementById('image');
-let round = document.getElementById('round');
-let newDiv = document.getElementById('Event');
-let EventDelete = document.getElementsByClassName('EventDelete');
+let form = document.getElementById("form");
+let content = document.getElementById("content");
+let title = document.getElementById("title");
+let img = document.getElementById("image");
+let round = document.getElementById("round");
+let newDiv = document.getElementById("Event");
+let EventDelete = document.getElementsByClassName("EventDelete");
 
-
-
-document.getElementById('form').addEventListener('submit', async function(event) {
+document
+  .getElementById("form")
+  .addEventListener("submit", async function (event) {
     event.preventDefault(); // preventing the default form behavior.
-    let newEvent = document.createElement('div');
-    let ImageDiv = document.createElement('div');
-    let EventDetailDiv = document.createElement('div');
-    let newRound = document.createElement('p');
-    let newTitle = document.createElement('h3');
-    let newContent = document.createElement('p');
-    let deleteButton = document.createElement('button');
-    let EventFooter = document.createElement('div');
+    let newEvent = document.createElement("div");
+    let ImageDiv = document.createElement("div");
+    let EventDetailDiv = document.createElement("div");
+    let newRound = document.createElement("p");
+    let newTitle = document.createElement("h3");
+    let newContent = document.createElement("p");
+    let deleteButton = document.createElement("button");
+    let EventFooter = document.createElement("div");
 
     newRound.innerText = "Round No: " + round.value;
     newTitle.innerText = title.value;
     newContent.innerText = content.value;
     deleteButton.textContent = "Delete Event";
-    deleteButton.setAttribute('class', 'btn btn-outline-dark mb-4');
+    deleteButton.setAttribute("class", "btn btn-outline-dark mb-4");
 
     newEvent.appendChild(ImageDiv);
     newEvent.appendChild(EventDetailDiv);
-    
+
     EventDetailDiv.appendChild(newTitle);
     EventDetailDiv.appendChild(newContent);
     EventDetailDiv.append(EventFooter);
@@ -35,28 +35,25 @@ document.getElementById('form').addEventListener('submit', async function(event)
     EventFooter.appendChild(newRound);
     EventFooter.appendChild(deleteButton);
 
+    newEvent.classList.add("EventStyle");
+    newContent.classList.add("EventContentStyle");
+    newTitle.classList.add("EventHeading");
+    newRound.classList.add("EventRound");
+    deleteButton.classList.add("EventDelete");
+    ImageDiv.classList.add("imageDiv");
+    EventDetailDiv.classList.add("EventDetailDiv");
+    EventFooter.classList.add("eventFooter");
 
+    EventDetailDiv.classList.add();
 
-    newEvent.classList.add('EventStyle');
-    newContent.classList.add('EventContentStyle');
-    newTitle.classList.add('EventHeading');
-    newRound.classList.add('EventRound');
-    deleteButton.classList.add('EventDelete');
-    ImageDiv.classList.add('imageDiv');
-    EventDetailDiv.classList.add('EventDetailDiv');
-    EventFooter.classList.add('eventFooter');
-   
-    EventDetailDiv.classList.add()
-
-
-    var file = document.getElementById('image').files[0];
+    var file = document.getElementById("image").files[0];
     if (file) {
       var reader = new FileReader();
-      reader.onload = function(e) {
+      reader.onload = function (e) {
         var image = new Image();
         image.src = e.target.result;
-        image.classList.add('EventImage'); // Optional: Add a CSS class for styling
-        
+        image.classList.add("EventImage"); // Optional: Add a CSS class for styling
+
         // newTitle.append(image);
         ImageDiv.append(image);
       };
@@ -66,12 +63,12 @@ document.getElementById('form').addEventListener('submit', async function(event)
     newDiv.appendChild(newEvent); // Append newEvent to newDiv
 
     form.reset();
-});
+  });
 
-newDiv.addEventListener('click', function(event) {
-  if (event.target && event.target.classList.contains('EventDelete')) {
-      let eventToDelete = event.target.parentElement.parentElement.parentElement;
-      console.log(eventToDelete);
-      newDiv.removeChild(eventToDelete);
+newDiv.addEventListener("click", function (event) {
+  if (event.target && event.target.classList.contains("EventDelete")) {
+    let eventToDelete = event.target.parentElement.parentElement.parentElement;
+    console.log(eventToDelete);
+    newDiv.removeChild(eventToDelete);
   }
 });
